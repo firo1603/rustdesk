dfd5e6a99f6723e4ad2a2ddca00a02b007b597b8
firo1603 <firo1603@gmail.com>
2025-11-20 17:23:21 +0800

    1.4.4

--- src/common.rs
+++ src/common.rs
@@ -1050,7 +1050,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdesk.firo1603.com".to_owned()
 }
 
 #[inline]
